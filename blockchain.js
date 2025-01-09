567b1f1b18b2eb0ea42761e68d131677985727c6
Santosh GS <santoshgs710@gmail.com>
2025-01-09 23:58:47 +0530

    Add blockchain.js

---
+++ blockchain.js
@@ -0,0 +1,83 @@
+const SHA256 = require("crypto-js/sha256");
+
+class Block {
+  constructor(index, timestamp, data, previousHash = "") {
+    this.index = index;
+    this.timestamp = timestamp;
+    this.data = data;
+    this.previousHash = previousHash;
+    this.hash = this.computeHash();
+    this.nonce = 0;
+  }
+
+  computeHash() {
+    return SHA256(
+      this.index +
+        this.previousHash +
+        this.timestamp +
+        JSON.stringify(this.data) +
+        this.nonce
+    ).toString();
+  }
+
+  proofOfWork(difficulty) {
+    while(
+      this.hash.substring(0, difficulty) != Array(difficulty + 1).join("0")
+    ) {
+      this.nonce++;
+      this.hash = this.computeHash();
+    }
+  }
+}
+
+class Blockchain {
+  constructor() {
+    this.chain = [this.createGenesisBlock()];
+    this.difficulty = 5;
+  }
+
+  createGenesisBlock() {
+    return new Block(0, "31/10/2008", "Genesis Block", "0"); // new Date().toString()
+  }
+
+  getLatestBlock() {
+    return this.chain[this.chain.length - 1];
+  }
+
+  addBlock(newBlock) {
+    newBlock.previousHash = this.getLatestBlock().hash;
+    newBlock.hash = newBlock.computeHash();
+    newBlock.proofOfWork(this.difficulty);
+    this.chain.push(newBlock);
+  }
+
+  isChainValid() {
+    for (let i = 1; i < this.chain.length; i++) {
+      const currentBlock = this.chain[i];
+      const previousBlock = this.chain[i - 1];
+
+      if (currentBlock.hash !== currentBlock.computeHash()) {
+        return false;
+      }
+
+      if (currentBlock.previousHash !== previousBlock.hash) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
+
+// let theblockchaincoder = new CryptoBlockchain();
+// console.log("theblockchaincoder mining in progress....");
+
+// theblockchaincoder.addNewBlock(
+//   new CryptoBlock(1, "01/06/2020", {
+//     sender: "Daulat Hussain ",
+//     recipient: "Shoaib Bhai",
+//     quantity: 50,
+//   })
+// );
+
+module.exports.Blockchain = Blockchain;
+module.exports.Block = Block;
